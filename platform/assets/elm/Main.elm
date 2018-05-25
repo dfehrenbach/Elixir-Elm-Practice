ba7e671e745ebfd92e45311b0fefced6e5fe5f25
Daniel Fehrenbach <dmf8646@rit.edu>
2018-05-25 12:44:36 -0500

    Add in character and items

--- platform/assets/elm/Main.elm
+++ platform/assets/elm/Main.elm
@@ -20,6 +20,7 @@ type alias Model =
 type alias Game =
     { description : String
     , featured : Bool
+    , slug : String
     , id : Int
     , thumbnail : String
     , title : String
@@ -119,9 +120,10 @@ decodeGamesList =
 
 decodeGame : Decode.Decoder Game
 decodeGame =
-    Decode.map5 Game
+    Decode.map6 Game
         (Decode.field "description" Decode.string)
         (Decode.field "featured" Decode.bool)
+        (Decode.field "slug" Decode.string)
         (Decode.field "id" Decode.int)
         (Decode.field "thumbnail" Decode.string)
         (Decode.field "title" Decode.string)
@@ -190,7 +192,11 @@ featured model =
                         [ h1 [] [ text "Featured" ]
                         , h2 [] [ text game.title ]
                         , p [] [ text game.description ]
-                        , button [ class "btn btn-lg btn-primary" ] [ text "Play Now!" ]
+                        , a
+                            [ class "btn btn-lg btn-primary"
+                            , href <| "games/" ++ game.slug
+                            ]
+                            [ text "Play Now!" ]
                         ]
                     ]
                 ]
@@ -217,7 +223,7 @@ gamesList games =
 
 gamesListItem : Game -> Html Msg
 gamesListItem game =
-    a [ href "#" ]
+    a [ href <| "games/" ++ game.slug ]
         [ li [ class "game-item media" ]
             [ div [ class "media-left" ]
                 [ img [ class "media-object", src game.thumbnail ] []
